0493accdcd00f5a482aaac7780b413af824ba468
mrLuisFer <mrluisfeer@gmail.com>
2021-12-04 14:40:48 -0600

    change the search state cause it wasn't work

--- components/Search/index.jsx
+++ components/Search/index.jsx
@@ -1,31 +1,27 @@
-import { useMemo, useRef, useState } from "react"
-import { createAutocomplete } from "@algolia/autocomplete-core"
-// import AutocompletePanel from "./AutocompletePanel"
-import AutocompleteItem from "./AutocompleteItem"
+import { useMemo, useRef, useState } from 'react'
+import { createAutocomplete } from '@algolia/autocomplete-core'
+import AutocompleteItem from './AutocompleteItem'
 
-export default function Search({ marginStyle = "mb-20", ...props }) {
+export default function Search(props) {
   const [autocompleteState, setAutocompleteState] = useState({
     collections: [],
     isOpen: false
   })
 
-  const autocomplete = useMemo(
-    () =>
-      createAutocomplete({
-        placeholder: "Busca tu oferta",
-        onStateChange: ({ state }) => setAutocompleteState(state),
-        getSources: () => [
-          {
-            sourceId: "offers-next-api",
-            getItems: async ({ query }) => {
-              if (!!query) return fetch(`/api/search?q=${query}`).then((res) => res.json())
-            }
-          }
-        ],
-        ...props
-      }),
-    [props]
-  )
+  const autocomplete = useMemo(() => createAutocomplete({
+    placeholder: 'Busca tu oferta',
+    onStateChange: ({ state }) => setAutocompleteState(state),
+    getSources: () => [{
+      sourceId: 'offers-next-api',
+      getItems: ({ query }) => {
+        if (!!query) {
+          return fetch(`/api/search?q=${query}`)
+            .then(res => res.json())
+        }
+      }
+    }],
+    ...props
+  }), [props])
 
   const formRef = useRef(null)
   const inputRef = useRef(null)
@@ -39,25 +35,29 @@ export default function Search({ marginStyle = "mb-20", ...props }) {
   })
 
   return (
-    <form ref={formRef} className={`flex justify-center ${marginStyle}`} {...formProps}>
-      <div className="flex relative p-1 bg-gradient-to-tr from-purple-600 to-blue-300 rounded-full w-2/6">
-        <input ref={inputRef} className="flex-1 p-2 pl-4 rounded-full w-full" {...inputProps} />
-        {autocompleteState.isOpen && (
-          <div className="absolute mt-16 top-0 left-0 border border-gray-100 bg-white overflow-hidden rounded-lg shadow-lg z-10" ref={panelRef} {...autocomplete.getPanelProps()}>
-            {autocompleteState.collections.map(({ items }, index) => (
-              <section key={`section-${index}`}>
-                {items.length > 0 && (
-                  <ul {...autocomplete.getListProps()}>
-                    {items.map((item) => (
-                      <AutocompleteItem key={item.id} {...item} />
-                    ))}
-                  </ul>
-                )}
-              </section>
-            )
-            )}
-          </div>
-        )}
+    <form ref={formRef} className="flex justify-center mb-20" {...formProps}>
+      <div className="flex relative p-1  bg-gradient-to-tr from-purple-600 to-blue-300 rounded-full w-2/6">
+        <input ref={inputRef} className='flex-1 p-2 pl-4 rounded-full w-full' {...inputProps} />
+        {
+          autocompleteState.isOpen && (
+            <div className="absolute mt-16 top-0 left-0 border border-gray-100 bg-white overflow-hidden rounded-lg shadow-lg z-10" ref={panelRef} {...autocomplete.getPanelProps()}>
+              {autocompleteState.collections.map((collection, index) => {
+                const { items } = collection
+                return (
+                  <section key={`section-${index}`}>
+                    {items.length > 0 && (
+                      <ul {...autocomplete.getListProps()}>
+                        {
+                          items.map(item => <AutocompleteItem key={item.id} {...item} />)
+                        }
+                      </ul>
+                    )}
+                  </section>
+                )
+              })}
+            </div>
+          )
+        }
       </div>
     </form>
   )
