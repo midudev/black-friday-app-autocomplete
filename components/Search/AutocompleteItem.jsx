c085e2c4e1e71fb867a70b982bb716c9312c16b4
mrLuisFer <mrluisfeer@gmail.com>
2021-12-04 14:38:40 -0600

    change double quotes to single

--- components/Search/AutocompleteItem.jsx
+++ components/Search/AutocompleteItem.jsx
@@ -4,11 +4,11 @@ const AutocompleteItem = ({ id, title, img, price }) => {
   return (
     <li>
       <Link href={`/detail/${id}`}>
-        <a className="hover:bg-blue-300 flex gap-4 p-4">
-          <img src={img} alt={title} className="w-12 h-12 object-contain" />
+        <a className='hover:bg-blue-300 flex gap-4 p-4'>
+          <img src={img} alt={title} className='w-12 h-12 object-contain' />
           <div>
-            <h3 className="text-sm font-semibold">{title}</h3>
-            <p className="text-xs text-gray-600">{price}</p>
+            <h3 className='text-sm font-semibold'>{title}</h3>
+            <p className='text-xs text-gray-600'>{price}</p>
           </div>
         </a>
       </Link>
